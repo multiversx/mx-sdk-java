cfbb3f6c23848cb5ab1edfb3c35e8dba0d9281dd
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2021-08-18 18:23:34 +0300

    added parsers for esdt transactions

---
+++ src/main/java/elrond/esdt/ESDTFunctions.java
@@ -0,0 +1,88 @@
+package elrond.esdt;
+
+import elrond.Address;
+import elrond.Exceptions;
+import elrond.Transaction;
+import elrond.esdt.dtos.ESDTNFTTransferTypes;
+import elrond.esdt.dtos.ESDTTransferTypes;
+
+import java.math.BigInteger;
+
+public class ESDTFunctions {
+    public static boolean isESDTTransfer(Transaction transaction) {
+        return transaction.getData().startsWith(ESDTConstants.ESDTTransferPrefix);
+    }
+
+    public static boolean isNFTTransfer(Transaction transaction) {
+        return transaction.getData().startsWith(ESDTConstants.ESDTNFTTransferPrefix);
+    }
+
+    public static ESDTTransferTypes extractESDTTransferTypes(Transaction transaction) throws Exceptions.InvalidESDTTransferPayload {
+        /*
+        Format:
+
+        Data: "ESDTTransfer" +
+          "@" + <token identifier in hexadecimal encoding> +
+          "@" + <value to transfer in hexadecimal encoding>
+         */
+
+        if (!isESDTTransfer(transaction)) {
+            throw new Exceptions.InvalidESDTTransferPayload();
+        }
+
+        String payload = transaction.getData();
+        String[] elements = payload.split(ESDTConstants.ScCallArgumentsSeparator);
+        if (elements.length < 3) {
+            throw new Exceptions.InvalidESDTTransferPayload();
+        }
+
+        String tokenIdentifier = elements[1];
+        BigInteger valueToTransfer = decodeHexEncodedBigInteger(elements[2]);
+
+        return new ESDTTransferTypes(
+                transaction.getSender(),
+                transaction.getReceiver(),
+                tokenIdentifier,
+                valueToTransfer
+        );
+    }
+
+    public static ESDTNFTTransferTypes extractESDTNFTTransferTypes(Transaction transaction) throws Exceptions.InvalidESDTNFTTransferPayload, Exceptions.AddressException {
+        /*
+        Format:
+
+        Data: "ESDTNFTTransfer" +
+          "@" + <token identifier in hexadecimal encoding> +
+          "@" + <the NFT nonce in hexadecimal encoding> +
+          "@" + <quantity to transfer in hexadecimal encoding> +
+          "@" + <destination address in hexadecimal encoding>
+         */
+
+        if (!isNFTTransfer(transaction)) {
+            throw new Exceptions.InvalidESDTNFTTransferPayload();
+        }
+
+        String payload = transaction.getData();
+        String[] elements = payload.split(ESDTConstants.ScCallArgumentsSeparator);
+        if (elements.length < 5) {
+            throw new Exceptions.InvalidESDTNFTTransferPayload();
+        }
+
+        String tokenIdentifier = elements[1];
+        long nonce = Long.parseLong(elements[2], 16);
+        BigInteger valueToTransfer = decodeHexEncodedBigInteger(elements[3]);
+        Address receiver = Address.fromHex(elements[4]);
+
+        return new ESDTNFTTransferTypes(
+                transaction.getSender(),
+                receiver,
+                tokenIdentifier,
+                valueToTransfer,
+                nonce
+        );
+    }
+
+    private static BigInteger decodeHexEncodedBigInteger(String number) {
+        return new BigInteger(number, 16);
+    }
+}
---
+++ src/test/java/elrond/esdt/ESDTFunctionsTest.java
@@ -0,0 +1,145 @@
+package elrond.esdt;
+
+import elrond.Address;
+import elrond.Exceptions;
+import elrond.Transaction;
+import elrond.esdt.dtos.ESDTNFTTransferTypes;
+import elrond.esdt.dtos.ESDTTransferTypes;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ESDTFunctionsTest {
+    @Test
+    public void shouldComputeESDTTransferType() {
+        Transaction tx = new Transaction();
+
+        tx.setData("random data");
+        assertFalse(ESDTFunctions.isESDTTransfer(tx));
+
+        tx.setData("");
+        assertFalse(ESDTFunctions.isESDTTransfer(tx));
+
+        tx.setData("ESDTTransfer@tkn@01");
+        assertTrue(ESDTFunctions.isESDTTransfer(tx));
+    }
+
+    @Test
+    public void shouldComputeESDTNFTTransferType() {
+        Transaction tx = new Transaction();
+
+        tx.setData("random data");
+        assertFalse(ESDTFunctions.isNFTTransfer(tx));
+
+        tx.setData("");
+        assertFalse(ESDTFunctions.isNFTTransfer(tx));
+
+        tx.setData("ESDTTransfer@tkn@01");
+        assertFalse(ESDTFunctions.isNFTTransfer(tx));
+
+        tx.setData("ESDTNFTTransfer@tkn@01@01@receiver");
+        assertTrue(ESDTFunctions.isNFTTransfer(tx));
+    }
+
+    @Test
+    public void shouldExtractESDTTransferTypes() throws Exceptions.AddressException, Exceptions.InvalidESDTTransferPayload {
+        Transaction tx = new Transaction();
+
+        Address sender = Address.fromHex("fd691bb5e85d102687d81079dffce842d4dc328276d2d4c60d8fd1c3433c3293");
+        Address receiver = Address.fromHex("c70cf50b238372fffaf7b7c5723b06b57859d424a2da621bcc1b2f317543aa36");
+        String tokenIdentifier = "tkn";
+        BigInteger valueToTransfer = new BigInteger("100");
+
+        // construct and test the data field
+        String txPayload = ESDTConstants.ESDTTransferPrefix +
+                ESDTConstants.ScCallArgumentsSeparator +
+                tokenIdentifier +
+                ESDTConstants.ScCallArgumentsSeparator +
+                valueToTransfer.toString(16);
+        assertEquals("ESDTTransfer@tkn@64", txPayload); // check the construction of the data field
+
+        tx.setSender(sender);
+        tx.setReceiver(receiver);
+        tx.setData(txPayload);
+
+        ESDTTransferTypes result = ESDTFunctions.extractESDTTransferTypes(tx);
+        assertEquals(tx.getSender(), result.getSender());
+        assertEquals(tx.getReceiver(), result.getReceiver());
+        assertEquals(tokenIdentifier, result.getTokenIdentifier());
+        assertEquals(valueToTransfer, result.getValueToTransfer());
+    }
+
+    @Test
+    public void shouldExtractESDTNFTTransferTypes() throws Exceptions.AddressException, Exceptions.InvalidESDTNFTTransferPayload {
+        Transaction tx = new Transaction();
+
+        Address sender = Address.fromHex("fd691bb5e85d102687d81079dffce842d4dc328276d2d4c60d8fd1c3433c3293");
+        Address receiver = Address.fromHex("c70cf50b238372fffaf7b7c5723b06b57859d424a2da621bcc1b2f317543aa36");
+        String tokenIdentifier = "tkn";
+        BigInteger valueToTransfer = new BigInteger("100");
+        long nonce = 20;
+
+        // construct and test the data field
+        String txPayload = ESDTConstants.ESDTNFTTransferPrefix +
+                ESDTConstants.ScCallArgumentsSeparator +
+                tokenIdentifier +
+                ESDTConstants.ScCallArgumentsSeparator +
+                Long.toString(nonce, 16) +
+                ESDTConstants.ScCallArgumentsSeparator +
+                valueToTransfer.toString(16) +
+                ESDTConstants.ScCallArgumentsSeparator +
+                receiver.hex();
+
+        assertEquals("ESDTNFTTransfer@tkn@14@64@c70cf50b238372fffaf7b7c5723b06b57859d424a2da621bcc1b2f317543aa36", txPayload); // check the construction of the data field
+
+        tx.setSender(sender);
+        tx.setReceiver(receiver);
+        tx.setData(txPayload);
+
+        ESDTNFTTransferTypes result = ESDTFunctions.extractESDTNFTTransferTypes(tx);
+        assertEquals(tx.getSender(), result.getSender());
+        assertEquals(tx.getReceiver().bech32(), result.getReceiver().bech32());
+        assertEquals(tokenIdentifier, result.getTokenIdentifier());
+        assertEquals(valueToTransfer, result.getValueToTransfer());
+        assertEquals(nonce, result.getNonce());
+    }
+
+    @Test
+    public void extractESDTTransferTypesShouldThrowExceptionsForBadPayload() {
+        assertThrows(Exceptions.InvalidESDTTransferPayload.class, () -> {
+            Transaction tx = new Transaction();
+            tx.setData("invalid esdt transfer payload");
+            ESDTFunctions.extractESDTTransferTypes(tx);
+        });
+
+        assertThrows(Exceptions.InvalidESDTTransferPayload.class, () -> {
+            Transaction tx = new Transaction();
+            tx.setData("ESDTTransfer@tkn"); // not enough arguments
+            ESDTFunctions.extractESDTTransferTypes(tx);
+        });
+    }
+
+    @Test
+    public void extractESDTNFTTransferTypesShouldThrowExceptionsForBadPayload() {
+        assertThrows(Exceptions.InvalidESDTNFTTransferPayload.class, () -> {
+            Transaction tx = new Transaction();
+            tx.setData("invalid nft transfer payload");
+            ESDTFunctions.extractESDTNFTTransferTypes(tx);
+        });
+
+        assertThrows(Exceptions.InvalidESDTNFTTransferPayload.class, () -> {
+            Transaction tx = new Transaction();
+            tx.setData("ESDTTransfer@tkn@04"); // esdt transfer, not nft
+            ESDTFunctions.extractESDTNFTTransferTypes(tx);
+        });
+
+        assertThrows(Exceptions.InvalidESDTNFTTransferPayload.class, () -> {
+            Transaction tx = new Transaction();
+            tx.setData("ESDTNFTTransfer@tkn@14@64"); // not enough arguments
+            ESDTFunctions.extractESDTNFTTransferTypes(tx);
+        });
+    }
+}
