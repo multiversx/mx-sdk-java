63b895e2505dc283edc9fc6ba5cfd51eddfb6d94
ssd04 <dariussuirab@gmail.com>
2022-10-04 12:02:50 +0300

    fixes after review: - added javadoc for public methods - added more unit tests

--- src/main/java/elrond/MessageSigning.java
+++ src/main/java/elrond/MessageSigning.java
@@ -5,13 +5,14 @@ import org.bouncycastle.crypto.params.Ed25519PrivateKeyParameters;
 import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
 import org.bouncycastle.crypto.signers.Ed25519Signer;
 import org.bouncycastle.util.encoders.Hex;
+import org.bouncycastle.util.encoders.DecoderException;
 
 public class MessageSigning {
-    static final String signerMessagePrefix = "\\x17Elrond Signed Message:\n";
-    private KeccakDigest hasher;
-    private byte[] privateKey;
+    private final String signerMessagePrefix = "\\x17Elrond Signed Message:\n";
+    private final KeccakDigest hasher;
+    private final byte[] privateKey;
 
-    public MessageSigning(String privateKeyHex) {
+    public MessageSigning(String privateKeyHex) throws DecoderException {
         this(Hex.decode(privateKeyHex));
     }
 
@@ -20,7 +21,14 @@ public class MessageSigning {
         this.hasher = createKeccakHasher();
     }
 
-    public String SignMessage(byte[] msg) {
+    /**
+     * It will sign the provided message with the internal stored private key.
+     *
+     * @param msg the message to be signed
+     *
+     * @return signature as hex encoded string
+     */
+    public String signMessage(byte[] msg) {
         Ed25519Signer signer = createEd25519Signer();
         byte[] message = computeHashOnMessage(msg);
 
@@ -30,25 +38,16 @@ public class MessageSigning {
         return new String(hexSig);
     }
 
-    private byte[] computeHashOnMessage(byte[] msg) {
-        final StringBuilder sb = new StringBuilder();
-
-        sb.append(signerMessagePrefix);
-        sb.append(msg.length);
-        for (byte b : msg) {
-            sb.append(b);
-        }
-
-        byte[] message = sb.toString().getBytes();
-
-        this.hasher.update(message, 0, message.length);
-        final byte[] marshalledMsg = new byte[this.hasher.getDigestSize()];
-        this.hasher.doFinal(marshalledMsg, 0);
-
-        return marshalledMsg;
-    }
-
-    public boolean VerifyMessageSignature(byte[] address, byte[] msg, String signature) {
+    /**
+     * Verify signature for a given message.
+     *
+     * @param address corresponding address which signed the message
+     * @param msg the message to be verified
+     * @param signature the provided signature
+     *
+     * @return true if signature match and false otherwise
+     */
+    public boolean verifyMessageSignature(byte[] address, byte[] msg, String signature) {
         Ed25519Signer signer = createEd25519Signer();
         byte[] message = computeHashOnMessage(msg);
         byte[] sig = Hex.decode(signature);
@@ -57,6 +56,28 @@ public class MessageSigning {
         signer.init(false, publicKeyParameters);
         signer.update(message, 0, message.length);
         return signer.verifySignature(sig);
+    }
+
+    private byte[] composeMessage(byte[] msg) {
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append(signerMessagePrefix);
+        sb.append(msg.length);
+        for (byte b : msg) {
+            sb.append(b);
+        }
+
+        return sb.toString().getBytes();
+    }
+
+    private byte[] computeHashOnMessage(byte[] msg) {
+        byte[] message = composeMessage(msg);
+
+        this.hasher.update(message, 0, message.length);
+        final byte[] marshalledMsg = new byte[this.hasher.getDigestSize()];
+        this.hasher.doFinal(marshalledMsg, 0);
+
+        return marshalledMsg;
     }
 
     private Ed25519Signer createEd25519Signer() {
--- src/test/java/elrond/MessageSigningTest.java
+++ src/test/java/elrond/MessageSigningTest.java
@@ -10,10 +10,30 @@ import org.bouncycastle.crypto.params.Ed25519KeyGenerationParameters;
 import org.bouncycastle.crypto.generators.Ed25519KeyPairGenerator;
 import org.bouncycastle.crypto.params.Ed25519PrivateKeyParameters;
 import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
+import org.bouncycastle.util.encoders.DecoderException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MessageSigningTest {
+
+    @Test
+    public void createMessageSigningFail() throws UnsupportedEncodingException {
+        assertThrows(DecoderException.class, () -> {
+            new MessageSigning((String) null);
+        });
+    }
+
+    @Test
+    public void signMessage() throws UnsupportedEncodingException {
+        String privateKeyHex = "06a180420e608220a6c2f997751a53f5bd3bbe63d36260a858a6d925daed593d";
+
+        MessageSigning messageSigner = new MessageSigning(privateKeyHex);
+        byte[] message = "message to sign".getBytes("utf-8");
+
+        String expectedSigHex = "c95ada60e1a58849234a5e95e0a80e149630585cb1b589c20cff09c283438e163c8a5bc595730538809bbcfbafbef2a991c74523268712d3220d636b60de8309";
+        String sigHex = messageSigner.signMessage(message);
+        assertEquals(expectedSigHex, sigHex);
+    }
 
     @Test
     public void signAndVerifyMessage() throws UnsupportedEncodingException {
@@ -27,9 +47,10 @@ public class MessageSigningTest {
         MessageSigning messageSigner = new MessageSigning(privatekey.getEncoded());
 
         byte[] message = "message to sign".getBytes("utf-8");
-        String sigHex = messageSigner.SignMessage(message);
+        String sigHex = messageSigner.signMessage(message);
+        assertNotNull(sigHex);
 
-        boolean verified = messageSigner.VerifyMessageSignature(publicKey.getEncoded(), message, sigHex);
+        boolean verified = messageSigner.verifyMessageSignature(publicKey.getEncoded(), message, sigHex);
         assertTrue(verified);
     }
 }
